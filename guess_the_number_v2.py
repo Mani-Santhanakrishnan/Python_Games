b8e661e1a4f16f6a46cd7d9b366ece25165ffa4e
Mani-Santhanakrishnan <66923487+Mani-Santhanakrishnan@users.noreply.github.com>
2020-06-16 19:25:14 +0100

    Add files via upload

---
+++ guess_the_number_v2.py
@@ -0,0 +1,27 @@
+# Number Guessing Game
+# Think of a number between 0 to 100. Program will find it ~8 attempts
+# Author : Mani Santhanakrishnan
+# Version: 3.8
+
+def get_string():
+    return input("Is the guess, correct/high/low:")
+
+numb=50
+lnum=0
+hnum=100
+print('The number is ',numb)
+feedb= get_string()
+
+while feedb != "correct":
+    if feedb== "high" :
+        hnum=numb
+        numb= int((lnum+hnum)/2)
+        print('The number is=', numb)
+        feedb = get_string()
+    elif feedb== "low" :
+        lnum=numb
+        numb= int((lnum+hnum)/2)
+        print('The number is=', numb)
+        feedb = get_string()
+            
+print('The number is =',numb)
